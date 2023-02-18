14a7d33ba866d3e3ba0e61a21b7bfc9f5183d393
vzavolsky <vzavolsky@ya.ru>
2023-02-19 01:32:31 +0300

    Howemwork #6 - Циклы

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         task_02();
         task_03();
         task_04();
+        task_05();
+        task_06();
+        task_07();
     }
     public static void task_01() {
         for (int i = 1; i <= 10; i++) {
@@ -26,6 +29,31 @@ public class Main {
     public static void task_04() {
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
+        }
+    }
+    public static void task_05() {
+        for (int year = 1904; year <= 2096; year++) {
+            boolean check400s = (year % 400) == 0;
+            boolean check100s = (year % 100) == 0;
+            boolean check4s = (year % 4) == 0;
+            if (check400s || (check4s && !check100s))
+                System.out.println(year + "-й год является високосным.");
+        }
+    }
+    public static void task_06() {
+        int res;
+        for (int i = 1; i <= 100; i++) {
+            res = i * 7;
+            System.out.println(res);
+            if (res >= 98) break;
+        }
+    }
+    public static void task_07() {
+        int increment = 1;
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(increment);
+            increment = increment * 2;
+            if (increment > 512) break;
         }
     }
 }
